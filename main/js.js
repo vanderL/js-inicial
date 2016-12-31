edf150017f8a80d58e2469efe2d6ac9371fa6507
vander <vnd.vander@gmail.com>
2016-12-31 03:17:43 -0200

    Commit da aula 3/7

--- main/js.js
+++ main/js.js
@@ -1,12 +1,17 @@
 //peso / altura * altura
 
-var peso = 100;
-var altura = 2;
+var setPeso = document.getElementById('peso-2');
+var setAltura = document.getElementById('altura-2');
 
-var ehDiferenteDeZero = (altura != 0);
-if (ehDiferenteDeZero) {
-	var imc = peso / (altura * altura);
+
+var paciente = {peso : setPeso.textContent, altura : setAltura.textContent};
+
+if (paciente.altura != 0) {
+	var imc = paciente.peso / (paciente.altura * paciente.altura);
+	var setIMC = document.getElementById("imc-2");
+	setIMC.textContent = imc;
 
 	console.log(imc);
-
+} else {
+	console.log("Entrada invalida! ")
 }
